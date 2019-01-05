574da7a49a174d0558325634351578e8d464e5c4
Gerd Reiss <gerdreiss@jscriptive.com>
2019-01-05 13:41:37 +0100

    starting and stopping actors

---
+++ udemy-akka-essentials/src/main/scala/faulttolerance/StartingStoppingActors.scala
@@ -0,0 +1,46 @@
+package faulttolerance
+
+import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
+
+object  StartingStoppingActors extends App {
+
+  case class StartChild(name: String)
+  case class StopChild(name: String)
+  case object Stop
+
+  class Parent extends Actor with ActorLogging  {
+    override def receive: Receive = withChildren(Map.empty)
+
+    def withChildren(children: Map[String, ActorRef]): Receive = {
+      case StartChild(name) =>
+        log.info(s"Starting child $name...")
+        context.become(withChildren(children + (name -> context.actorOf(Props[Child], name))))
+      case StopChild(name) =>
+        log.info(s"Stopping child $name...")
+        children.get(name).foreach(context.stop)
+      case Stop =>
+        log.info("Stopping...")
+        context.stop(self)
+    }
+  }
+  class Child extends Actor with ActorLogging {
+    override def receive: Receive = {
+      case message => log.info(message.toString)
+    }
+  }
+
+  val system = ActorSystem("StartingStoppingActors")
+  val parent = system.actorOf(Props[Parent], "parent")
+
+  parent ! StartChild("child1")
+  val child1 = system.actorSelection("/user/parent/child1")
+  child1 ! "Hi, kid!"
+
+  parent ! StopChild("child1")
+
+  parent ! StartChild("child2")
+  val child2 = system.actorSelection("/user/parent/child2")
+  child2 ! "Hi, kid!"
+
+  parent ! Stop
+}
