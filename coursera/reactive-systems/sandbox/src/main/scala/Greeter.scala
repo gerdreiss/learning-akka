aaff6f0b322750ad5926407c64e638f84b67cb24
Gerd Reiss <gerd@reiss.pro>
2022-02-01 12:16:31 +0100

    Coursera: Reactive Systems - Guardians

--- coursera/reactive-systems/sandbox/src/main/scala/Greeter.scala
+++ coursera/reactive-systems/sandbox/src/main/scala/Greeter.scala
@@ -1,16 +1,33 @@
-import akka.actor.typed.Behavior
+import akka.actor.typed.*
 import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.FSM.Shutdown
 
 enum Greeter:
   case Greet(whom: String)
   case Stop
 
+enum Guardian:
+  case NewGreeter(replyTo: ActorRef[ActorRef[Greeter]])
+  case Shutdown
+
 val greeter: Behavior[Greeter] =
   Behaviors.receiveMessage[Greeter] {
-    case Greeter.Stop        =>
-      println("shutting down...")
-      Behaviors.stopped
     case Greeter.Greet(whom) =>
       println(s"Hello from Typed Actor 'greeter', $whom!")
       Behaviors.same
+
+    case Greeter.Stop =>
+      println("shutting down...")
+      Behaviors.stopped
   }
+
+val guardian = Behaviors.receive[Guardian] {
+  case (ctx, Guardian.NewGreeter(replyTo)) =>
+    val ref: ActorRef[Greeter] = ctx.spawnAnonymous(greeter)
+    replyTo ! ref
+    Behaviors.same
+
+  case (_, Guardian.Shutdown) =>
+    Behaviors.stopped
+
+}
