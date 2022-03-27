da15f1c9b78b020852f6e518b41100d2784fa86f
Gerd Reiss <gerd@reiss.pro>
2022-03-27 15:06:52 +0200

    [Rock the JVM] Akka Streams with Scala 3 - Operator Fusion

--- udemy/akka-streams-scala3/src/main/scala/section02/OperatorFusion.scala
+++ udemy/akka-streams-scala3/src/main/scala/section02/OperatorFusion.scala
@@ -11,13 +11,13 @@ object OperatorFusion extends App:
   given system: ActorSystem             = ActorSystem("OperatorFusion")
   given materializer: ActorMaterializer = ActorMaterializer()
 
-  val numSource   = Source(1 to 1000)
-  val incFlow     = Flow[Int].map(_ + 1)
-  val dblFlow     = Flow[Int].map(_ * 2)
-  val printerSink = Sink.foreach[Int](println)
+  val numSource    = Source(1 to 100)
+  val incFlow      = Flow[Int].map(_ + 1)
+  val dblFlow      = Flow[Int].map(_ * 2)
+  val printingSink = Sink.foreach[Int](println)
 
   // running on the same actor => operator/component Fusion
-  // numSource.via(incFlow).via(dblFlow).to(printerSink).run()
+  // numSource.via(incFlow).via(dblFlow).to(printingSink).run()
 
   // more or less same as the graph above
   class SimpleActor extends Actor:
@@ -44,4 +44,32 @@ object OperatorFusion extends App:
     x * 2
   }
 
-  numSource.via(longIncFlow).via(longDblFlow).to(printerSink).run()
+  // numSource.via(longIncFlow).via(longDblFlow).to(printingSink).run()
+
+  // async boundary
+  numSource
+    .via(longIncFlow)
+    .async            // runs on one actor
+    .via(longDblFlow)
+    .async            // runs on another actor
+    .to(printingSink) // runs on third actor
+  // .run()
+
+  // ordering guarantees
+  numSource
+    .map { element =>
+      println(s"Flow A: $element")
+      element
+    }
+    .async
+    .map { element =>
+      println(s"Flow B: $element")
+      element
+    }
+    .async
+    .map { element =>
+      println(s"Flow C: $element")
+      element
+    }
+    .async
+    .runWith(Sink.ignore)
